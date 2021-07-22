8cdf2395dcb2bdf4ec2fb29b42a2c242c0426610
jmkhan99 <jmkhana@gmail.com>
2021-07-23 00:26:03 +0900

    20210723 ie 10

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 "use strict";
 
-const footerBtn = document.getElementsByClassName("footer__button");
-const footerIcon = document.getElementsByClassName("footer__icon");
-const mainItemsList = document.getElementsByClassName("main__items__list");
-const mainItems = document.getElementsByClassName("main__items");
-const footerInput = document.getElementsByClassName("footer__input");
-const mainItemsListIcon = document.getElementsByClassName(
+var footerBtn = document.getElementsByClassName("footer__button");
+var footerIcon = document.getElementsByClassName("footer__icon");
+var mainItemsList = document.getElementsByClassName("main__items__list");
+var mainItems = document.getElementsByClassName("main__items");
+var footerInput = document.getElementsByClassName("footer__input");
+var mainItemsListIcon = document.getElementsByClassName(
   "main__items__list__icon"
 );
 
 footerBtn[0].addEventListener("click", createBlock);
-footerInput[0].addEventListener("keypress", (event) => {
+footerInput[0].addEventListener("keypress", function (event) {
   console.log("Enter");
   if (event.key === "Enter") {
     createBlock();
@@ -23,23 +23,23 @@ function createBlock() {
     return;
   }
   console.log("btn click");
-  const createdLi = document.createElement("li");
+  var createdLi = document.createElement("li");
   console.log(createdLi);
   createdLi.setAttribute("class", "main__items__list");
   mainItems[0].appendChild(createdLi);
-  const createdSpan = document.createElement("span");
+  var createdSpan = document.createElement("span");
   createdSpan.setAttribute("class", "main__items__list__span");
-  let footerInputValue = footerInput[0].value;
+  var footerInputValue = footerInput[0].value;
   createdSpan.innerText = footerInputValue;
   createdLi.appendChild(createdSpan);
-  const createdBtn = document.createElement("button");
+  var createdBtn = document.createElement("button");
   createdBtn.setAttribute("class", "main__items__button");
   createdLi.appendChild(createdBtn);
-  createdBtn.addEventListener("click", () => {
+  createdBtn.addEventListener("click", function () {
     console.log("delete");
     mainItems[0].removeChild(createdLi);
   });
-  const createdIcon = document.createElement("i");
+  var createdIcon = document.createElement("i");
   createdIcon.setAttribute("class", "fas fa-trash-alt main__items__list__icon");
   createdBtn.appendChild(createdIcon);
   footerInput[0].value = "";
