8aa24a4f279f16fa3a0298cb82a37107ab926c8e
jmkhan99 <jmkhana@gmail.com>
2021-07-23 16:27:29 +0900

    20210723 dream coding shopping list localStorage

--- script.js
+++ script.js
@@ -8,41 +8,70 @@ const footerInput = document.getElementsByClassName("footer__input");
 const mainItemsListIcon = document.getElementsByClassName(
   "main__items__list__icon"
 );
+let localToDos = JSON.parse(localStorage.getItem("toDos"));
 
-footerBtn[0].addEventListener("click", createBlock);
+let toDoList = [];
+
+if (localToDos) {
+  let toDoList = [];
+  toDoList = localToDos;
+  toDoList.forEach(createBlock);
+}
+
+footerBtn[0].addEventListener("click", handleCreateBlock);
 footerInput[0].addEventListener("keypress", (event) => {
   console.log("Enter");
   if (event.key === "Enter") {
-    createBlock();
+    handleCreateBlock();
   }
 });
 
-function createBlock() {
+function handleCreateBlock() {
   if (footerInput[0].value === "") {
     console.log("return");
     return;
   }
+  const inputValueObj = {
+    text: footerInput[0].value,
+    id: Date.now(),
+  };
+  createBlock(inputValueObj);
+}
+
+function createBlock(inputValue) {
   console.log("btn click");
   const createdLi = document.createElement("li");
+  createdLi.id = inputValue.id;
+  console.log(createdLi.id);
   console.log(createdLi);
   createdLi.setAttribute("class", "main__items__list");
   mainItems[0].appendChild(createdLi);
   const createdSpan = document.createElement("span");
   createdSpan.setAttribute("class", "main__items__list__span");
-  let footerInputValue = footerInput[0].value;
-  createdSpan.innerText = footerInputValue;
+  createdSpan.innerText = inputValue.text;
   createdLi.appendChild(createdSpan);
   const createdBtn = document.createElement("button");
   createdBtn.setAttribute("class", "main__items__button");
   createdLi.appendChild(createdBtn);
-  createdBtn.addEventListener("click", () => {
-    console.log("delete");
-    mainItems[0].removeChild(createdLi);
-  });
   const createdIcon = document.createElement("i");
   createdIcon.setAttribute("class", "fas fa-trash-alt main__items__list__icon");
   createdBtn.appendChild(createdIcon);
+  toDoList.push(inputValue);
+  console.log(toDoList);
   footerInput[0].value = "";
   footerInput[0].focus();
   createdLi.scrollIntoView();
+  localStorage.setItem("toDos", JSON.stringify(toDoList));
+  createdIcon.addEventListener("click", removeLi);
+}
+
+function removeLi(event) {
+  mainItems[0].removeChild(event.target.parentNode.parentNode);
+  const newToDoList = toDoList.filter(function (item) {
+    return item.id !== parseInt(event.target.parentNode.parentNode.id);
+  });
+  console.log(parseInt(event.target.parentNode.parentNode.id));
+  console.log(newToDoList);
+  localStorage.setItem("toDos", JSON.stringify(newToDoList));
+  toDoList = newToDoList;
 }
